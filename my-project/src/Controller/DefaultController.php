6942b1a75144d11a6edcc315b763b793232c0f03
Franco Rodriguez <40544712+Kultera@users.noreply.github.com>
2018-11-24 18:14:04 -0300

    pase las rta

--- my-project/src/Controller/DefaultController.php
+++ my-project/src/Controller/DefaultController.php
@@ -17,10 +17,21 @@ class DefaultController extends AbstractController
     {	
     	$Archivo_parseado = Yaml::parseFile(__DIR__.'/preguntas.yml');
         $Preguntas = $Archivo_parseado["preguntas"];
+        $cant_preguntas=count($Preguntas);
+        $cant_preguntas=$cant_preguntas-1;
+
 
     	//dump($Archivo_parseado);
+        shuffle($Preguntas);
+        $RTA=[];
+        foreach ($Preguntas as $pregunta ) {
 
+            $mezclar_preg = array_merge($pregunta["respuestas_correctas"],$pregunta["respuestas_incorrectas"]);
+            shuffle($mezclar_preg);
+            $RTA[]=$mezclar_preg;
+
+        }
         return $this->render('default/index.html.twig', [
-            'preguntas' => $Preguntas]);
+            'preguntas' => $Preguntas,"cant_preguntas" => $cant_preguntas,"RTA" => $RTA]);
     }
 }
