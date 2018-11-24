ff0a245d3760991ab4068015cc3b90bc6a94e77e
Franco Rodriguez <40544712+Kultera@users.noreply.github.com>
2018-11-24 16:19:27 -0300

    se muestra la informacion

--- my-project/src/Controller/DefaultController.php
+++ my-project/src/Controller/DefaultController.php
@@ -16,10 +16,11 @@ class DefaultController extends AbstractController
     public function index()
     {	
     	$Archivo_parseado = Yaml::parseFile(__DIR__.'/preguntas.yml');
-    	// dump($Archivo_parseado["preguntas"][0]);
-        dump($Archivo_parseado);
+        $Preguntas = $Archivo_parseado["preguntas"];
 
+    	//dump($Archivo_parseado);
 
-        return $this->render('default/index.html.twig', ['controller_name' => 'DefaultController',] , array('pregunta' => $Archivo_parseado);
+        return $this->render('default/index.html.twig', [
+            'preguntas' => $Preguntas]);
     }
 }
